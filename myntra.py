2fd83a82da24d96dd5b12d6f350b6490792e2e88
Perakachinmai <chinmaiperaka@gmail.com>
2025-07-26 10:52:28 +0530

    myntra

--- myntra.py
+++ myntra.py
@@ -6,8 +6,8 @@ brand_name='ziva'
 product_decs="womens maxi dress"
 product_rating=4.5
 product_price=1500
-product_sizes=['s','m','l','xl']
-product_sizes=[28,30,32,34]
+product_sizes=['s','m','l','xl','xxl']
+product_sizes=[28,30,32,34,36,38]
 print(type)
 product_colors=['red','blue','black']
 
