71a4f5aeb0421909fbfdc3f7ac61863c1df04716
Perakachinmai <chinmaiperaka@gmail.com>
2025-07-30 11:27:40 +0530

    control statements

--- control statements.py
+++ control statements.py
@@ -1,1 +1,9 @@
-print(hi)
+#if condition
+value=10
+
+#check if given value is +ve
+if value>0:
+#print(f"Given {value}is Positive") #IndentationError: expected an indented block after 'if' statement on line 5
+    print(f"Given {value} is positive")
+ # print("Code will not execute") #IndentationError: unindent does not match any outer indentation level
+    print("code will execute")
